5029eec78fa09b38705f746c40fd5314fe28f1cc
otherw1se <artursmulko@gmail.com>
2015-06-28 23:53:59 +0100

    Added PageList More data to seed for testing purposes Added pagination for test data Added filtering and sorting for test data Added coffee page

--- Indepp/Controllers/HomeController.cs
+++ Indepp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace Indepp.Controllers
 {
@@ -14,6 +15,46 @@ namespace Indepp.Controllers
         public ActionResult Index()
         {
             return View(db.Places.ToList());
+        }
+
+        public ActionResult Coffee(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.IDSortParam = sortOrder == "ID" ? "id_desc" : "ID";
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+            ViewBag.CurrentFilter = searchString;
+
+            var places = db.Places.AsQueryable();
+
+            if (!String.IsNullOrEmpty(searchString))
+                places = places.Where(p => p.Name.Contains(searchString));
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    places = places.OrderByDescending(p => p.Name);
+                    break;
+                case "id_desc":
+                    places = places.OrderByDescending(p => p.ID);
+                    break;
+                case "ID":
+                    places = places.OrderBy(p => p.ID);
+                    break;
+                default:
+                    places = places.OrderBy(p => p.Name);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
+            return View(places.ToPagedList(pageNumber, pageSize));
         }
     }
 }
--- Indepp/DAL/PlaceInitializer.cs
+++ Indepp/DAL/PlaceInitializer.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace Indepp.DAL
 {
-    public class PlaceInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<PlaceContext>
+    public class PlaceInitializer : System.Data.Entity.DropCreateDatabaseAlways<PlaceContext>
     {
         protected override void Seed(PlaceContext context)
         {
@@ -16,7 +16,14 @@ namespace Indepp.DAL
 
             var places = new List<Place> 
             {
-                new Place {ID = 1, Name = "La Bottega Milanese", Category = "Coffee Shop", Address = address, Article = article, Description = description }
+                new Place {ID = 1, Name = "La Bottega Milanese", Category = "Coffee Shop", Address = address, Article = article, Description = description },
+                new Place {ID = 2, Name = "Test Coffee", Category = "Coffee Shop", Address = address, Article = article, Description = description },
+                new Place {ID = 3, Name = "Good Coffee", Category = "Coffee Shop", Address = address, Article = article, Description = description },
+                new Place {ID = 4, Name = "Shit Coffee", Category = "Coffee Shop", Address = address, Article = article, Description = description },
+                new Place {ID = 5, Name = "The Old Black Cat", Category = "Coffee Shop", Address = address, Article = article, Description = description },
+                new Place {ID = 6, Name = "Name me what you like", Category = "Coffee Shop", Address = address, Article = article, Description = description },
+                new Place {ID = 7, Name = "Burning hell", Category = "Coffee Shop", Address = address, Article = article, Description = description },
+                new Place {ID = 8, Name = "Sweet lords coffee", Category = "Coffee Shop", Address = address, Article = article, Description = description },
             };
 
             places.ForEach(p => context.Places.Add(p));
