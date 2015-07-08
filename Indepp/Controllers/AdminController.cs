11882bce3f6d098f964ebc648c266193b3a6d94b
otherw1se <artursmulko@gmail.com>
2015-07-08 23:52:25 +0100

    Added place list to Admin View - Fixed sorting not working on other pages

--- Indepp/Controllers/AdminController.cs
+++ Indepp/Controllers/AdminController.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Indepp.DAL;
+using PagedList;
 
 namespace Indepp.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        public PlaceContext Context;
+
+        public AdminController(PlaceContext context)
         {
-            return View();
+            Context = context;
+        }
+        // GET: Admin
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.IDSortParam = sortOrder == "ID" ? "id_desc" : "ID";
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+            ViewBag.CurrentFilter = searchString;
+
+            var places = Context.Places.AsQueryable();
+
+            if (!String.IsNullOrEmpty(searchString))
+                places = places.Where(p => p.Name.Contains(searchString));
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    places = places.OrderByDescending(p => p.Name);
+                    break;
+                case "id_desc":
+                    places = places.OrderByDescending(p => p.ID);
+                    break;
+                case "ID":
+                    places = places.OrderBy(p => p.ID);
+                    break;
+                default:
+                    places = places.OrderBy(p => p.Name);
+                    break;
+            }
+
+            int pageSize = 20;
+            int pageNumber = (page ?? 1);
+
+            return View(places.ToPagedList(pageNumber, pageSize));
         }
     }
 }
