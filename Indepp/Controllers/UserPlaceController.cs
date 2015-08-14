b62fa4445422e542d7e455a56ed2837a4a7275cf
otherw1se <artursmulko@gmail.com>
2015-08-14 12:02:55 +0300

    Added initial user place controller

---
+++ Indepp/Controllers/UserPlaceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Indepp.Models;
+
+namespace Indepp.Controllers
+{
+    public class UserPlaceController : Controller
+    {
+        // GET: UserPlace
+        public ActionResult CreatePlace(Place? place)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreatePlace(Place place)
+        {
+            return View();
+        }
+
+        public ActionResult PreviewPlace(Place place)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PreviewPlace(Place place)
+        {
+            return View(); // return a thank you page or thank you text.
+        }
+    }
+}
