1d7d046084d2a0bd46000cf875f6629a6b07527d
otherw1se <artursmulko@gmail.com>
2015-08-17 22:42:25 +0100

    First bunch of work on user Contribution - Added views - Changed Action Methods - Added Contribute link - Added some sample text

---
+++ Indepp/Controllers/ContributeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Indepp.Models;
+
+namespace Indepp.Controllers
+{
+    public class ContributeController : Controller
+    {
+        // GET: UserPlace
+        public ActionResult CreatePlace()
+        {
+            var workingHours = new WorkingHour();
+            var place = new Place() { WorkingHours = workingHours.PopulateHours() };
+            return View(place);
+        }
+
+        [HttpPost]
+        public ActionResult PreviewPlace(Place place)
+        {
+            return View(place);
+        }
+
+        [HttpPost]
+        public ActionResult SubmitPlace(Place place)
+        {
+            return View(); // return a thank you page or thank you text.
+        }
+    }
+}
